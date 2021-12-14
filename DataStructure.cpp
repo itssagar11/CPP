7552618bc8edc99d298719c44f27438b297c1fd2
itssagar11 <64058503+itssagar11@users.noreply.github.com>
2021-12-14 12:40:40 +0530

    DataStructure  dynamic Impementation in CPP

--- DataStructure.cpp
+++ DataStructure.cpp
@@ -14,8 +14,56 @@ class Node{
 			next=NULL;
 		}
 };
+class Collection{
+    public:
+    // function to find middle element
+    Node* getMid(Node *h){
+        Node* p1=h;
+        Node* p2=h;
+        while(p2->next!=NULL && (p2->next)->next!=NULL){
+            p2=(p2->next)->next;
+            p1=p1->next;
+        }
+        return p1;
+    }
+    // function to merge two list
+    Node* Merge(Node *left,Node *right){
+        if(left==NULL){
+            return right;
+        }
+        if(right==NULL){
+            return left;
+        }
+        if(left->data<right->data){
+            left->next=Merge(left->next,right);
+            return left;
+        }
+            right->next=Merge(left,right->next);
+            return right;
+    }
+
+
+};
+class Sorting:public Collection{
+public:
+    // function of merge sort in linkedList.
+    Node* mergeSort(Node *head)
+    {
+       if(head==NULL || head->next==NULL){
+            return head;
+        }
+        Node* mid=getMid(head);
+        Node* midNext=mid->next;
+        mid->next=NULL;
+        Node* left=mergeSort(head);
+        Node* right=mergeSort(midNext);
+        return Merge(left,right);
+
+    }
+} ;
 // class LinkedList.
 class LinkedList{
+    protected:
 	Node* head;
 	public:
 		LinkedList(){
@@ -28,7 +76,7 @@ class LinkedList{
 			head=n;
 			return;
 		}
-		n->next=*head;
+		n->next=head;
 		head=n;
 		return;
 	}
@@ -78,14 +126,14 @@ class LinkedList{
 		Node* todel=NULL;
 		// case 1: If element is first;
 		if((head)->data==n){
-			todel= *head;
-			*head=(head)->next;
+			todel= head;
+			head=(head)->next;
 			return;
 		}
 		// case 2: if  not a first element.
 		Node *itr=head;
 		int flag=0;
-		
+
 		while(itr!=NULL && flag==0){
 			if((itr->next)->data==n){
 				todel=itr->next;
@@ -98,10 +146,10 @@ class LinkedList{
 			return;
 		}
 		itr->next=todel->next;
-		delete itr;	
+		delete itr;
 	}
 		// function to print element.
-	void Print(Node *head){
+	void Print(){
 		if(head==NULL){
 			cout<<"List Is Empty"<<endl;
 			return;
@@ -111,5 +159,120 @@ class LinkedList{
 			head=head->next;
 		}
 	}
-	
+
 };
+// Stack using Linkedlist (Dynamic stack);
+// Stack class inherit linkedList class;
+class Stack: public LinkedList{
+    public:
+      Stack(){
+      }
+      void PUSH(int n){
+          InsertFirst(n);
+      }
+      void POP(){
+        DeleteFirst();
+      }
+      void PEEK(){
+        if(head==NULL){
+            cout<<"Stack is Empty"<<endl;
+            return;
+        }
+        int d=head->data;
+        cout<<d<<endl;
+      }
+};
+class Queue{
+    protected:
+    Node * fro_nt;
+    Node* rear;
+    public:
+    Queue(){
+        fro_nt=NULL;
+        rear=NULL;
+    }
+    void enqueue(int n){
+        Node* temp= new Node(n);
+        if(fro_nt==NULL){
+            fro_nt=rear=temp;
+            return;
+        }
+        rear->next=temp;
+        rear=rear->next;
+        return;
+    }
+    void dequeue(){
+        if(fro_nt==NULL){
+            cout<<"Queue is Empty"<<endl;
+            return;
+        }
+        Node* todel= fro_nt;
+        if(fro_nt==rear){
+            fro_nt=rear=NULL;
+        }else
+            fro_nt=fro_nt->next;
+        delete todel;
+    }
+    void print(){
+    Node *itr= fro_nt;
+        while(itr!=NULL){
+            cout<<itr->data<<endl;
+            itr=itr->next;
+        }
+
+    }
+
+
+};
+class CircularQueue: public Queue{
+    public:
+       void enqueue(int v)
+        {
+            Node* temp = new Node(v);
+            if (fro_nt == NULL)
+                fro_nt =rear = temp;
+            else
+                rear->next = temp;
+            rear = temp;
+            temp=fro_nt;
+        }
+
+        void deQueue()
+        {
+            if (fro_nt == NULL) {
+                cout<<"Queue is empty"<<endl;
+                return;
+            }
+            Node* todel;
+            // If the last node to be deleted
+            if (fro_nt == rear) {
+                todel=fro_nt;
+                fro_nt = NULL;
+                rear = NULL;
+            }
+            else // There are more than one nodes
+            {
+                todel= fro_nt;
+                fro_nt = fro_nt->next;
+                rear->next = fro_nt;
+
+            }
+            delete todel;
+        }
+
+        // to display a queue
+        void print(){
+            if(fro_nt==NULL){
+                cout<<"Queue is Empty"<<endl;
+                return;
+            }
+            Node* temp = fro_nt;
+            while (fro_nt->next != fro_nt) {
+                cout<<temp->data<<endl;
+                temp = temp->next;
+            }
+               cout<<temp->data<<endl;
+
+        }
+};
+
